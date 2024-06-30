330cba393bf2101c7d2bf50c4b2153e8d6e6a020
Pulpogar <aeg2k@hotmail.com>
2024-06-30 17:01:59 -0300

    Agregado front basico

--- db/scripts/update_img.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `moviesdb`.`movies` SET `MovieImage` = '3.jpg' WHERE (`MovieID` = '3');
-UPDATE `moviesdb`.`movies` SET `MovieImage` = '2.jpg' WHERE (`MovieID` = '2');
-UPDATE `moviesdb`.`movies` SET `MovieImage` = '1.jpg' WHERE (`MovieID` = '1');
