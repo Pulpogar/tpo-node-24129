848bc852e7ba6b4ebd9fe1cbca501f3543494035
Pulpogar <aeg2k@hotmail.com>
2024-07-01 09:38:28 -0300

    db remota

--- db/db.js
+++ db/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
+// const dbConfig = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'sasa',
+//   database: 'moviesdb'
+// };
+
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'sasa',
-  database: 'moviesdb'
+  host: 'mysql-cyberex.alwaysdata.net',
+  user: 'cyberex',
+  password: 'sa-cyberex9184',
+  database: 'cyberex_moviesdb'
 };
 
 // Creo la conexión a la base de datos con promesas
